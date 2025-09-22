84423fa57378d4157a3e190eab2bcc1afabddae4
Jan Zukowski <janzukowski@proton.me>
2025-09-22 18:32:52 +0200

    chore: set content-type header to application/xml in GET /channels/<id> handler

--- backend/src/routes/channels/get_channel.rs
+++ backend/src/routes/channels/get_channel.rs
@@ -1,21 +1,19 @@
-use rocket::{http::Status, response::status};
+use rocket::{http::{ContentType, Status}};
 
 #[rocket::get("/<channel_id>")]
 pub async fn controller(
     channel_id: u32,
     config: &rocket::State<crate::config::Config>
-) -> rocket::response::status::Custom<String> {
+) -> (Status, (ContentType, String))  {
     let path_to_read = format!("{}/channel-{}.xml", config.data_path.trim_end_matches("/"), channel_id);
     let content = match std::fs::read_to_string(&path_to_read) {
         Ok(content) => content,
         Err(_) => {
-            let body = format!("cannot read this channel from {}", path_to_read);
-
-            return status::Custom(Status::NotFound, body);
+            return (Status::Ok, (ContentType::Text, format!("cannot read this channel from {}", path_to_read)));
         }
     };
 
-    return status::Custom(Status::Ok, content);
+    return (Status::Ok, (ContentType::XML, content));
 }
 
 #[derive(serde::Deserialize)]
